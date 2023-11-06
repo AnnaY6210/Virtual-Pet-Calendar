a784903c7c2f8696ed26e5ec07fce6bbbf0fb16d
Pamela Xie <pjxie@ucsc.edu>
2023-11-06 13:58:08 -0800

    Button for claiming task rewards

--- public/app.py
+++ public/app.py
@@ -9,8 +9,7 @@ app = Flask(__name__)
 app.secret_key = "password"
 
 # Initialize Firestore DB
-default_app = initialize_app()
-db = firestore.client()
+# default_app = initialize_app()
 config = {
     "apiKey": "AIzaSyD-gV3V7Gn1KAPrZSI64H2fDVz2xR4NkuM",
     "authDomain": "virtual-pet-calendar.firebaseapp.com",
@@ -31,7 +30,7 @@ person = {"is_logged_in": False, "name": "", "email": "", "uid": ""}
 
 @app.route("/")
 def index():
-    if not session["person"]["is_logged_in"]:
+    if not session.get("person") or not session["person"].get("is_logged_in"):
         return redirect(url_for("login"))
     if "credentials" not in session:
         return redirect(url_for("oauth2callback"))
@@ -72,12 +71,9 @@ def calendar():
     http_auth = credentials.authorize(httplib2.Http())
     service = discovery.build("calendar", "v3", http=http_auth)
 
-    # get all events from calendar
-    # target_date = datetime.datetime(2023, 10, 25)  # Change to your desired date
-
-    # Define the start and end times for the target day
-    # start_time = target_date.replace(hour=0, minute=0, second=0).isoformat() + "Z"
-    # end_time = target_date.replace(hour=23, minute=59, second=59).isoformat() + "Z"
+    # Code for test user generation
+    # data = {"name": "Mortimer 'Morty' Smith", "balance": 0, "prev_claim":"2023-10-27T19:01:04.425925"}
+    # print(db.child("users").child("test_user_id").set(data))
 
     now = datetime.datetime.utcnow().isoformat() + "Z"
 
@@ -126,29 +122,33 @@ def calendar():
     # Tasks
     service = discovery.build("tasks", "v1", http=http_auth)
     results = service.tasklists().list().execute()
-    prev_login = datetime.datetime(2023, 10, 27)  # Placeholder Time
+
+    # TODO: Access current user's login
+    prev_claimstr = db.child("users").child("test_user_id").get().val()["prev_claim"]
+    prev_claim = datetime.datetime.fromisoformat(prev_claimstr)
     tasksDates = {}
     tasklists = results.get("items", [])
+    tmrw = (datetime.datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)).isoformat() + "Z"
+    display_min = (datetime.datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0) - datetime.timedelta(days=10)).isoformat() + "Z"
 
     for item in tasklists:
-        pastTasks = (
+        task_display = (
             service.tasks()
             .list(
                 tasklist=item["id"],
-                dueMin=prev_login.isoformat() + "Z",
-                dueMax=now,
+                dueMin=display_min,
+                dueMax=tmrw,
+                maxResults=10,
                 showHidden=True,
             )
             .execute()
         )
-        todayTasks = (
-            service.tasks()
-            .list(tasklist=item["id"], dueMin=now, dueMax=now, showHidden=True)
-            .execute()
-        )
 
-        tlist = pastTasks.get("items", []) + todayTasks.get("items", [])
-        for task in tlist:
+        # TODO: Access current user's balance
+        current_balance = db.child("users").child("test_user_id").get().val()["balance"]
+        # Always display at least some tasks
+        displayed_tasks = task_display.get("items", [])
+        for task in displayed_tasks:
             date = datetime.datetime.strptime(task["due"], "%Y-%m-%dT%H:%M:%S.%fZ")
             formattedDate = date.strftime("%D")
 
@@ -156,21 +156,69 @@ def calendar():
                 status = "Completed"
             else:
                 status = "Incomplete"
-
+            
             if not tasksDates.get(formattedDate):
                 tasksDates[formattedDate] = []
             tasksDates[formattedDate].append([task["title"], status])
-    return render_template("calendar.html", dates=dates, tasksDates=tasksDates)
+
+    return render_template("calendar.html",
+                            dates=dates, 
+                            tasksDates=dict(sorted(tasksDates.items(), reverse = True)), 
+                            balance = current_balance,
+                            prev_claim = prev_claim.strftime("%D"))
+
+@app.route('/claim_tasks')
+def claim_tasks():
+    if "credentials" not in session:
+        return redirect(url_for("oauth2callback"))
+    credentials = client.OAuth2Credentials.from_json(session["credentials"])
+    if credentials.access_token_expired:
+        return redirect(url_for("oauth2callback"))
+    http_auth = credentials.authorize(httplib2.Http())
+    service = discovery.build("tasks", "v1", http=http_auth)
+    results = service.tasklists().list().execute()
+    tasklists = results.get("items", [])
+
+    today = (datetime.datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)).isoformat()
+    prev_claimstr = db.child("users").child("test_user_id").get().val()["prev_claim"]
+    money_gained = 0
+    for item in tasklists:
+        past_completed_tasks = (
+            service.tasks()
+            .list(
+                tasklist=item["id"],
+                dueMin=prev_claimstr + "Z",
+                dueMax=today + "Z",
+                showHidden=True,
+            )
+            .execute()
+        )
+        task_list = past_completed_tasks.get("items", [])
+        # TODO: Get current user's balance
+        current_balance = db.child("users").child("test_user_id").get().val()["balance"]
+        for task in task_list:
+            date = datetime.datetime.strptime(task["due"], "%Y-%m-%dT%H:%M:%S.%fZ")
+            if task["status"] == "completed":
+                completion_date = datetime.datetime.strptime(task["completed"], "%Y-%m-%dT%H:%M:%S.%fZ")
+                due_date = datetime.datetime.strptime(task["due"], "%Y-%m-%dT%H:%M:%S.%fZ")
+                if (completion_date - due_date).days < 1:
+                    money_gained += 100
+    # TODO: Update values for current user
+    db.child("users").child("test_user_id").update({"balance": current_balance + money_gained})
+    db.child("users").child("test_user_id").update({"prev_claim": today})
+    return ('', 204)
 
 
 @app.route("/inv")
 def inventory():
-    return render_template("inventory.html")
+    current_balance = db.child("users").child("test_user_id").get().val()["balance"]
+    return render_template("inventory.html", balance = current_balance)
 
 
 @app.route("/shop")
 def shop():
-    return render_template("shop.html")
+    current_balance = db.child("users").child("test_user_id").get().val()["balance"]
+    return render_template("shop.html", balance = current_balance)
 
 
 @app.route("/login")
@@ -233,6 +281,7 @@ def register():
                 # Get the name of the user
                 "name": name,
             }
+            print()
             # Append data to the firebase realtime database
             data = {"name": name, "email": email}
             db.child("users").child(person["uid"]).set(data)
@@ -246,7 +295,7 @@ def register():
         if session.get("person") and session["person"]["is_logged_in"]:
             return redirect(url_for("index"))
         else:
-            return redirect(url_for("register"))
+            return redirect(url_for("signup"))
 
 
 if __name__ == "__main__":
