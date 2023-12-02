5773a7e124c8b034c757ca5de099a45fa1d353f9
Kevin Huang <hhuan135@ucsc.edu>
2023-12-01 20:32:38 -0800

    original code didn't start and listen to port

--- public/app.py
+++ public/app.py
@@ -362,4 +362,4 @@ def register():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
